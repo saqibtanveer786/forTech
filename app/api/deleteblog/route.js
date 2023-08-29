76b306ac3a77e3cd90d8e0232b7780643175b414
Saqib Tanveer <adward797@gmail.com>
2023-08-28 21:35:08 -0700

    switched from fs post.. and pri..  deletepostroute

--- app/api/deleteblog/route.js
+++ app/api/deleteblog/route.js
@@ -1,22 +1,30 @@
 const { NextRequest, NextResponse } = require('next/server');
-const fs = require('fs');
+import prisma from '../../../lib/prisma';
 
 async function DELETE(request) {
   try {
     const url = new URL(request.url);
     const searchParams = new URLSearchParams(url.search);
-    const slug = searchParams.get('slug');
+    const id = parseInt(searchParams.get('id'))
 
-    const filesList = await fs.readdirSync('blogs', 'utf-8');
-    if (!filesList.includes(`${slug}.json`)) {
-      return NextResponse.json('There is no such blog to delete');
-    }
-    console.log(filesList);
-    const file = await fs.unlinkSync(`blogs/${slug}.json`);
-    console.log(file);
-    return NextResponse.json({}, { status: 200 });
+    // deleting post
+    const deletePost = await prisma.post.delete({
+      where: {
+        id: id
+      }
+    })
+
+    return NextResponse.json(
+      { deletePost, message: "Post Deleted Successfully" } || { message: "Error while deleting" },
+      { status: deletePost ? 200 : 404 }
+    );
+
   } catch (error) {
     console.log(error);
+    return NextResponse.json(
+      { message: "An Unexpected Error Occured" },
+      { status: 500 }
+    );
   }
 }
 
