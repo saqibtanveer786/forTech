cf53c1b879e224b47e6cc1e289ac732ed0e8174b
Saqib Tanveer <adward797@gmail.com>
2023-09-02 00:10:41 -0700

    send image along with other blog data

--- app/pages/addblog/page.js
+++ app/pages/addblog/page.js
@@ -14,19 +14,29 @@ export default function Addblog(pl) {
 
 
   // Function for getting input fields data
-  function getData(e) {
-    // e.preventDefault();
-    // if (e.target.name === 'image') {
-    //   setData({ ...data, image: e.target.files[0] })
-    //   return
-    // }
-    // setData({ ...data, [e.target.name]: e.target.value });
-    // console.log(data)
+  async function getData(e) {
+    e.preventDefault();
+    if (e.target.name === 'image') {
+      // Reading file with FileReader()
+      const file = document.getElementsByName("image")[0].files[0]
+      const reader = new FileReader();
+      reader.readAsDataURL(file)
+      let base64Image
+      reader.onload = async function () {
+        base64Image = await reader.result.split(',')[1]; // Extract the Base64 data
+        setData({ ...data, image: base64Image })
+        console.log(data)
+      }
+      return
+    }
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data)
   }
 
 
   // Publish a blog
   async function pusblishBlog() {
+    data.content = content
     const url = 'http://localhost:3000/api/addblog';
     const response = await fetch(url, {
       cache: 'no-cache',
@@ -34,7 +44,7 @@ export default function Addblog(pl) {
       headers: {
         'content-type': 'application/json',
       },
-      body: formData,
+      body: JSON.stringify(data),
     });
 
     const jsonresponse = await response.json();
@@ -42,36 +52,20 @@ export default function Addblog(pl) {
   }
 
   // Uload image function
-  async function uploadImage() {
-    // const form = await document.getElementsByTagName("form")[0]
-    // const ress = await fetch('http://localhost:3000/api/imageupload', {
-    //   cache: 'no-cache',
-    //   method: 'post',
-    //   headers: {
-    //     'Content-Type': `multipart/form-data; boundary=${formD._boundary}`,
-    //   },
-    //   body: formD,
-    // })
+  // async function uploadImage() {
+  //   // Send the Base64 image to your Next.js API route
+  //   const response = await fetch('http://localhost:3000/api/imageupload', {
+  //     cache: 'no-cache',
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({ image: base64Image }),
+  //   });
 
-    const file = document.getElementsByName("image")[0].files[0]
-    const reader = new FileReader();
-    reader.readAsDataURL(file)
-    reader.onload = async function () {
-      const base64Image = reader.result.split(',')[1]; // Extract the Base64 data
-
-      // Send the Base64 image to your Next.js API route
-      const response = await fetch('http://localhost:3000/api/imageupload', {
-        cache: 'no-cache',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ image: base64Image }),
-      });
-
-      // Handle the API response
-    };
-  }
+  // Handle the API response
+  // };
+  // }
 
   return (
     <>
@@ -96,26 +90,16 @@ export default function Addblog(pl) {
         name='briefdescription'
         onChange={getData}
       />
-
-      <form action="">
-        <input
-          type="file"
-          id="base-input"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-6"
-          placeholder='Front Image'
-          autoComplete='off'
-          required
-          name='image'
-        // onChange={(e) => {
-        //   e.preventDefault()
-        //   uploadImage(e.target)
-        // }}
-        />
-        <input type='submit' placeholder='submit' onClick={e => {
-          e.preventDefault()
-          uploadImage()
-        }} />
-      </form>
+      <input
+        type="file"
+        id="base-input"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-6"
+        placeholder='Front Image'
+        autoComplete='off'
+        required
+        name='image'
+        onChange={getData}
+      />
       <JoditEditor
         value={content}
         tabIndex={1}
