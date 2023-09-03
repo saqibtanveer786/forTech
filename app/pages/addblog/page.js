5d49f295de6ed5cac0598822feef0fb91ce32bb0
Saqib Tanveer <adward797@gmail.com>
2023-09-03 03:04:02 -0700

    worked on add blog page designing

--- app/pages/addblog/page.js
+++ app/pages/addblog/page.js
@@ -34,7 +34,8 @@ export default function Addblog(pl) {
   }
 
   return (
-    <>
+    <section className='w-[90%] mx-auto my-14'>
+      <h1 className='text-2xl font-bold'>Add Blog</h1>
       <input
         type="text"
         id="base-input"
@@ -79,6 +80,6 @@ export default function Addblog(pl) {
         type="submit"
         className="mt-14 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >Publish</button>
-    </>
+    </section>
   );
 }
