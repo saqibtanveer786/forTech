f0d9514c92645f070a8aa4dc0e57bc39ab34f093
Saqib Tanveer <adward797@gmail.com>
2023-09-13 01:00:15 -0700

    added real info on contact page

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -14,30 +14,26 @@ export default function Contact() {
   return (
     <>
       <section className="">
+        {/* Container */}
         <div className="container mx-auto flex h-full justify-center mt-8 relative">
+
+          {/* main section */}
           <section className="flex flex-col md:flex-row gap-4 p-4 w-full justify-center">
-            <div className="bg-blue-900 rounded-2xl flex flex-col px-8 justify-center items-center md:items-start order-2 md:order-1 md:rounded-tl-2xl md:rounded-bl-2xl">
-              <h1 className="text-4xl mt-8 md:mt-0 border-b-2 md:border-b-0 text-gray-50">Contact Us</h1>
 
+            {/* Contact Info email and number */}
+            <div className="bg-blue-900 rounded-2xl flex flex-col px-8 justify-center items-center md:items-start order-2 md:order-1 md:rounded-tl-2xl md:rounded-bl-2x py-14">
+              <h1 className="text-4xl border-b-2 md:border-b-0 text-gray-50">Contact Us</h1>
               <div className="mt-8 flex flex-col w-full gap-8">
-                <div className="flex w-full gap-1 items-center text-xl md:text-sm  text-gray-100">
-
-                  23 Avenue Park ,Paris
-                </div>
-
                 <div className="flex gap-1 items-center text-xl md:text-sm text-gray-100">
-
-                  mail@blog.com
+                  adward797@gmail.com
                 </div>
                 <div className="flex gap-1 items-center text-xl md:text-sm text-gray-100">
-
-                  +91 8858856668
+                  +92 0318-8205462
                 </div>
-              </div>
-              <div className="flex gap-8  mt-8 mb-8 md:mb-0 items-start">
-
               </div>
             </div>
+
+            {/* Contact Form */}
             <div className="md:w-1/2 order-1">
               <h2 className="text-4xl font-bold mb-4 tracking-wider text-blue-600">Get In Touch</h2>
               <form className="w-full" >
@@ -78,7 +74,9 @@ export default function Contact() {
                 >Send</button>
               </form>
             </div>
+
           </section>
+
         </div>
       </section>
     </>
