c06fd7808e4ef0a51d7b4c48cc4b11f9a755fbc3
Saqib Tanveer <adward797@gmail.com>
2023-09-17 07:37:28 -0700

    made a signin page

---
+++ app/pages/(auth)/signin/page.js
@@ -0,0 +1,31 @@
+"use client"
+import React from 'react'
+
+// Importing nextjs components
+import Image from 'next/image'
+
+// Importing logos
+import { logos } from '../../../../lib/logos'
+
+export default function page() {
+    return (
+        <section className="bg-gray-50 ">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Image src={'/logo.png'} alt='forTech' width={150} height={150} className='my-2' priority />
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
+                            Continue With
+                        </h1>
+                    </div>
+                    <div class="flex items-center justify-center min-h-[200px] bg-gray-100 px-4">
+                        <button class="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 w-full justify-center">
+                            {logos.google()}
+                            <span>Continue with Google</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
