c8c3dde4233fcc0e11a8527c7a5805e85794ca23
Saqib Tanveer <adward797@gmail.com>
2023-10-24 10:12:18 -0700

    displayed category list on all blogs page

--- app/pages/blogs/page.js
+++ app/pages/blogs/page.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 // Importing components
 import Post from '../../../components/Post'
+import PostCategories from '../../../components/PostCategories'
 
 // Importing server actions
 import { getBlogs } from '../../../lib/serverAction'
+import { getAuthSession } from '../../../lib/auth'
 
 export default async function Page() {
     const blogs = await getBlogs()
+    const session = await getAuthSession()
     return (
-        <>
-            {
-                blogs && blogs.map(blog => {
-                    return <Post key={blog?.id} blog={blog} />
-                })
-            }
-        </>
+        <div className='flex flex-col justify-start max-w-7xl mx-auto -mt-6'>
+            <PostCategories />
+            <div className='-mt-7 flex-1'>
+                {
+                    blogs && blogs.map(blog => {
+                        return <Post key={blog?.id} blog={blog} session={session} />
+                    })
+                }
+            </div>
+        </div>
     )
 }
--- components/PostCategories.js
+++ components/PostCategories.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-export default function PostCategories() {
+export default function PostCategories({ className }) {
     return (
-        <div className="my-8 ">
-            <p className="text-xl font-semibold border-l-4 border-gray-800 pl-2 text-gray-700">Explore Categories</p>
-            <ul className="ml-3 text-lg font-semibold text-gray-700">
-                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm flex justify-between">Javascript</li>
-                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm flex justify-between">Node Js</li>
-                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm flex justify-between">React Js</li>
-                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm flex justify-between">CSS</li>
+        <div className="my-8 mx-2">
+            <ul className="ml-3 text-lg font-semibold text-gray-700 flex flex-row flex-1 overflow-auto">
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Javascript</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Node Js</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">React Js</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Artificial Intelligence</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Cloud Computing</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Amazon</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">News</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Next Js</li>
+                <li className="border py-2 px-2 mt-2 mb-1 cursor-pointer hover:text-gray-900 rounded-sm text-center min-w-fit">Programming</li>
             </ul>
         </div>
     )
