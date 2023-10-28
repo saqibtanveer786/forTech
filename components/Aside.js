95b3b66eca99f8d714d66ef1be169d6f8e132250
Saqib Tanveer <adward797@gmail.com>
2023-10-28 03:58:30 -0700

    put related post on blogpost page

--- components/Aside.js
+++ components/Aside.js
@@ -12,6 +12,8 @@ export default function Aside() {
             {/* AboutAuthor */}
             <AboutAuthor />
 
+            {/* Related Posts */}
+            <RelatedPost />
         </aside>
     )
 }
--- components/RelatedPost.js
+++ components/RelatedPost.js
@@ -6,43 +6,45 @@ export default function RelatedPost() {
 
         <div id="related-posts" className="grid gap-2">
             <p className="text-3xl font-semibold border-l-4 my-8 border-gray-800 pl-2 text-gray-700">Related Posts</p>
-            <div className="grid grid-cols-2 border rounded-lg">
-                <div className=" md:col-span-2 lg:col-span-1">
-                    <Image src="/img/aaa.png" alt='image' className="w-full h-auto" height={500} width={500} />
+            <div className='flex flex-wrap gap-6'>
+                <div className="grid grid-cols-1 border rounded-lg p-3 max-w-[270px]">
+                    <div className="w-fit">
+                        <Image src="/img/general.webp" alt='image' height={250} width={250} />
+                    </div>
+                    <div className="px-2 flex flex-col justify-start pt-4">
+                        <p className="text-xl cursor-pointer">
+                            <a href="single.html">
+                                While Loop in Javascript
+                            </a>
+                        </p>
+                        <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
+                    </div>
                 </div>
-                <div className="col-span-1 md:col-span-2 lg:col-span-1 px-2 flex flex-col justify-center">
-                    <p className="text-xl cursor-pointer">
-                        <a href="single.html">
-                            While Loop in Javascript
-                        </a>
-                    </p>
-                    <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
+                <div className="grid grid-cols-1 border rounded-lg p-3 max-w-[270px]">
+                    <div className="w-fit">
+                        <Image src="/img/general.webp" alt='image' height={250} width={250} />
+                    </div>
+                    <div className="px-2 flex flex-col justify-start pt-4">
+                        <p className="text-xl cursor-pointer">
+                            <a href="single.html">
+                                Foreach Loop in Javascript
+                            </a>
+                        </p>
+                        <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
+                    </div>
                 </div>
-            </div>
-            <div className="grid grid-cols-2 border rounded-lg">
-                <div className=" md:col-span-2 lg:col-span-1">
-                    <Image src="/img/aaa.png" alt='image' className="w-full h-auto" height={500} width={500} />
-                </div>
-                <div className="col-span-1 md:col-span-2 lg:col-span-1 px-2 flex flex-col justify-center">
-                    <p className="text-xl cursor-pointer">
-                        <a href="single.html">
-                            Foreach Loop in Javascript
-                        </a>
-                    </p>
-                    <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
-                </div>
-            </div>
-            <div className="grid grid-cols-2 border rounded-lg">
-                <div className=" md:col-span-2 lg:col-span-1">
-                    <Image src="/img/aaa.png" alt='image' className="w-full h-auto" height={500} width={500} />
-                </div>
-                <div className="col-span-1 md:col-span-2 lg:col-span-1 px-2 flex flex-col justify-center">
-                    <p className="text-xl cursor-pointer">
-                        <a href="single.html">
-                            Map Loop in Javascript
-                        </a>
-                    </p>
-                    <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
+                <div className="grid grid-cols-1 border rounded-lg p-3 max-w-[270px]">
+                    <div className="w-fit">
+                        <Image src="/img/general.webp" alt='image' height={250} width={250} />
+                    </div>
+                    <div className="px-2 flex flex-col justify-start pt-4">
+                        <p className="text-xl cursor-pointer">
+                            <a href="single.html">
+                                Map Loop in Javascript
+                            </a>
+                        </p>
+                        <p className="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. m, sapiente explicabo!</p>
+                    </div>
                 </div>
             </div>
         </div>
