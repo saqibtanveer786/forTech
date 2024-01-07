67e68ee2dac648fb9e8f7f58d7764e3f492e94e7
saqibtanveer786 <adward797@gmail.com>
2024-01-07 06:56:54 -0500

    fixed the liked disliked

--- app/api/(comments$voting)/votes/addvote/route.js
+++ app/api/(comments$voting)/votes/addvote/route.js
@@ -2,113 +2,157 @@ import { NextResponse } from "next/server";
 import prisma from "lib/prisma";
 
 async function deleteVote(userId, postId) {
-    const delete_vote = await prisma.Vote.delete({
-        where: {
-            userId_postId: {
-              userId,
-              postId
-            },
-    }
-})
-    if(!delete_vote) return false;
-    return true;
+  const delete_vote = await prisma.Vote.delete({
+    where: {
+      userId_postId: {
+        userId,
+        postId,
+      },
+    },
+  });
+  if (!delete_vote) return false;
+  return true;
 }
 
 export async function POST(request) {
-    try {
-    const {userId, postId, type} = await request.json();
+  try {
+    const { userId, postId, type } = await request.json();
 
     // check for body data
-    if(!userId || !postId || !type) return NextResponse.json({message: "something is wrong", status: 'error'},{status: 405})
+    if (!userId || !postId || !type)
+      return NextResponse.json(
+        { message: "something is wrong", status: "error" },
+        { status: 405 }
+      );
 
     // check if vote of user allready exists
     const checkVote = await prisma.vote.findUnique({
-        where: {
-            userId_postId: {
-                userId,
-                postId
-            }
-        }
-    })
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
 
     // check if user already liked or disliked
-    if(checkVote && checkVote.type === type){
-        return NextResponse.json({message: "vote already exists.", status: 'error'}, {status: 405})
+    if (checkVote && checkVote.type === type) {
+      const delVote = await deleteVote(userId, postId);
+
+      if (!delVote)
+        return NextResponse.json(
+          { message: "Internal server error", status: "error" },
+          { status: 405 }
+        );
+
+      return NextResponse.json(
+        { message: "Vote Deleted", status: "success" },
+        { status: 405 }
+      );
     }
 
     // case in which user trying to change the vote type
-    if(checkVote && checkVote.type !== type){
+    if (checkVote && checkVote.type !== type) {
+      const updateVote = await prisma.vote.update({
+        where: {
+          userId_postId: {
+            userId,
+            postId,
+          },
+        },
+        data: {
+          type,
+        },
+      });
 
-        const updateVote = await prisma.vote.update({
-            where: {
-                userId_postId: {
-                    userId,
-                    postId
-                }
-            },
-            data: {
-                type
-            }
-        })
-        
-        if (!updateVote) return NextResponse.json({message: "Internal server error", status: 'error'}, {status: 405})
+      if (!updateVote)
+        return NextResponse.json(
+          { message: "Internal server error", status: "error" },
+          { status: 405 }
+        );
 
-        return NextResponse.json({message: "operation successfull", status: 'success', type: updateVote.type}, {status: 200})
+      return NextResponse.json(
+        {
+          message: "operation successfull",
+          status: "success",
+          type: updateVote.type,
+        },
+        { status: 200 }
+      );
     }
 
     // case for first time
     const addTheNewOne = await prisma.Vote.create({
-        data: {
-            userId,
-            postId,
-            type
-        }
-    })
-    
+      data: {
+        userId,
+        postId,
+        type,
+      },
+    });
+
     // vote submitt validity
-    if (!addTheNewOne) return NextResponse.json({message: "Internal server error", status: 'error'}, {status: 405})
+    if (!addTheNewOne)
+      return NextResponse.json(
+        { message: "Internal server error", status: "error" },
+        { status: 405 }
+      );
 
     // response on operation complete
-    return NextResponse.json({message: "Thanks for your opinion!", status: 'success', type: addTheNewOne.type}, {status: 200})
+    return NextResponse.json(
+      {
+        message: "Thanks for your opinion!",
+        status: "success",
+        type: addTheNewOne.type,
+      },
+      { status: 200 }
+    );
 
     // catch blog
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({message: "Internal server error", status: 'error'}, {status: 500})
-    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Internal server error", status: "error" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function PUT(request) {
-    try {
-        const {userId, postId} = await request.json();
+  try {
+    const { userId, postId } = await request.json();
 
-        console.log(userId, postId)
+    console.log(userId, postId);
 
-        const findVote = await prisma.vote.findUnique({
-            where: {
-                userId_postId: {
-                    userId,
-                    postId
-                }
-            },
-            select: {
-                type: true
-            }
-        })
+    const findVote = await prisma.vote.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+      select: {
+        type: true,
+      },
+    });
 
-        if(!findVote) return NextResponse.json({
-            message: "Internal server error"
-        }, {status: 500})
+    if (!findVote)
+      return NextResponse.json(
+        {
+          message: "Internal server error",
+        },
+        { status: 500 }
+      );
 
-        console.log(findVote)
+    console.log(findVote);
 
-       return NextResponse.json(
-        {type: findVote.type}, {status: 200})
-        
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({
-            message: "Internal server error"
-        }, {status: 500})
-    }
+    return NextResponse.json({ type: findVote.type }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
 }
--- components/BlogPostDetailedPage/LikeDislikeBtns.js
+++ components/BlogPostDetailedPage/LikeDislikeBtns.js
@@ -1,59 +1,67 @@
-"use client"
-import { addVote } from 'lib/serverAction';
-import React, { useEffect, useState } from 'react'
+"use client";
+import { addVote } from "lib/serverAction";
+import React, { useEffect, useState } from "react";
 // react icons
 import { AiOutlineLike } from "react-icons/ai";
 import { AiOutlineDislike } from "react-icons/ai";
 
-export default function LikeDislikeBtns({userId, postId}) {
-
-  const [voteType, setVoteType] = useState()
+export default function LikeDislikeBtns({ userId, postId }) {
+  const [voteType, setVoteType] = useState();
 
   async function handleVote(type) {
-      if(voteType === type) return;
-      const response = await addVote(userId, postId, type);
-      console.log(response)
-      setVoteType(()=> response.type)
+    const response = await addVote(userId, postId, type);
+    console.log(response);
+    setVoteType(() => response.type);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     async function toCall() {
-      const url = `http://localhost:3000/api/votes/addvote`
+      const url = `http://localhost:3000/api/votes/addvote`;
       const response = await fetch(url, {
-        method: 'PUT',
-        cache: 'no-cache',
+        method: "PUT",
+        cache: "no-cache",
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            userId,
-            postId,
-        })
-    })
+          userId,
+          postId,
+        }),
+      });
       const jsonResponse = await response.json();
-      setVoteType(()=> jsonResponse.type)
+      setVoteType(() => jsonResponse.type);
     }
-    
+
     toCall();
-  }, [])
+  }, []);
 
   return (
-    <div className='container my-4 flex flex-col'>
-      <h2 className='text-xl font-light'>How would you thing about this post</h2>
-        <div id='btn-container' className='flex justify-start my-4'>
-          <button 
-          className={`bg-gray-300 text-blue-500 mx-2 p-3 rounded-full ${voteType === 'UP'? 'text-gray-300 bg-blue-500': 'hover:bg-gray-200'}`}
-          onClick={()=>handleVote('UP')}          
-          >
-              <AiOutlineLike size={40} />
-          </button>
-          <button 
-          className={`bg-gray-300 text-blue-500 mx-2 p-3 rounded-full ${voteType === 'DOWN'? 'text-gray-300 bg-blue-500': 'hover:bg-gray-200'}`}
-          onClick={()=>handleVote('DOWN')}          
-          >
-              <AiOutlineDislike size={40} />
-          </button>
-        </div>
+    <div className="container my-4 flex flex-col">
+      <h2 className="text-xl font-light">
+        How would you thing about this post
+      </h2>
+      <div id="btn-container" className="flex justify-start my-4">
+        <button
+          className={`mx-2 p-3 rounded-full${
+            voteType === "UP"
+              ? " text-gray-300 bg-blue-500"
+              : " hover:bg-gray-200 bg-gray-300 text-blue-500 "
+          }`}
+          onClick={() => handleVote("UP")}
+        >
+          <AiOutlineLike size={40} />
+        </button>
+        <button
+          className={`mx-2 p-3 rounded-full${
+            voteType === "DOWN"
+              ? " text-gray-300 bg-blue-500"
+              : " hover:bg-gray-200 bg-gray-300 text-blue-500 "
+          }`}
+          onClick={() => handleVote("DOWN")}
+        >
+          <AiOutlineDislike size={40} />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
