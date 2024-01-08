1dcc9f447e301cbd4eff0a1920378f6d8ec5137c
saqibtanveer786 <adward797@gmail.com>
2024-01-08 11:07:07 -0500

    little fixes

--- components/Author/FollowersFollowing.js
+++ components/Author/FollowersFollowing.js
@@ -4,11 +4,11 @@ export default function FollowersFollowing({ followers, following }) {
   return (
     <div className="py-4 flex justify-center items-center w-full divide-x divide-gray-400 divide-solid">
       <span className="text-center px-2">
-        <span className="font-bold text-gray-700">{followers.length}</span>
+        <span className="font-bold text-gray-700">{followers?.length}</span>
         <span className="text-gray-600"> followers</span>
       </span>
       <span className="text-center px-2">
-        <span className="font-bold text-gray-700">{following.length}</span>
+        <span className="font-bold text-gray-700">{following?.length}</span>
         <span className="text-gray-600"> following</span>
       </span>
     </div>
