c233f0713583714822ab901495f4c06674f4520f
saqibtanveer786 <adward797@gmail.com>
2024-01-11 08:25:41 -0500

    little improvement

--- components/HomePage/FeaturedPosts.js
+++ components/HomePage/FeaturedPosts.js
@@ -13,22 +13,20 @@ export default async function FeaturedPosts() {
   const blogs = await getBlogs();
   const session = await getServerSession(authOptions);
   return (
-    <>
-      <section className="max-w-7xl mx-auto">
-        <h2 className="col-span-full text-xl md:text-xl mb-8 border-l-4 border-blue-800 pl-2 my-4 text-blue-800 ">
-          Featured posts
-        </h2>
-        {blogs &&
-          blogs.map((blog) => (
-            <Post key={blog?.id} blog={blog} session={session} />
-          ))}
-      </section>
+    <section className="max-w-7xl mx-auto">
+      <h2 className="col-span-full text-xl md:text-xl mb-8 border-l-4 border-blue-800 pl-2 my-4 text-blue-800 ">
+        Featured posts
+      </h2>
+      {blogs &&
+        blogs.map((blog) => (
+          <Post key={blog?.id} blog={blog} session={session} />
+        ))}
       <Link
         href="/blogs"
-        className="text-blue-600 block text-center my-10 bg-gray-100 rounded-md max-w-4xl mx-auto py-4"
+        className="text-blue-600 block text-center my-10 bg-gray-100 rounded-md max-w-4xl mx-auto py-4 hover:bg-gray-200"
       >
         See All ...
       </Link>
-    </>
+    </section>
   );
 }
